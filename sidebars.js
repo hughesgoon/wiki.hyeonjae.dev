b63aeaac3caaf4c5b9c74d0640a014ffa8d0a8a5
Hyeonjae.kim <hyeonjae.dev@gmail.com>
2024-12-07 21:09:31 +0900

    post: Publish index at ai-statistics and one post at ai-statistics/basic

--- sidebars.js
+++ sidebars.js
@@ -17,12 +17,14 @@ module.exports = {
       type: 'category',
       label: 'AI를 위한 수리통계학',
       items: [
+        'math-stats/ai-statistics/index',
         {
           type: 'category',
           label: '확률 기초',
           items: [
             'math-stats/ai-statistics/basic/probability',
             'math-stats/ai-statistics/basic/conditional-probability-and-bayes-theorem',
+            'math-stats/ai-statistics/basic/random-variable-and-distribution',
           ],
         },
       ],
